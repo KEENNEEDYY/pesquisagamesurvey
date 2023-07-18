f21ff3b69916964e02f3683c080d06aca0c6ca88
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-07-17 23:54:08 -0300

    Project created

---
+++ backend/src/main/java/com/desenvolvedorkennedy/pesquisagamesurvey/PesquisagamesurveyApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvedorkennedy.pesquisagamesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisagamesurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisagamesurveyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/desenvolvedorkennedy/pesquisagamesurvey/PesquisagamesurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolvedorkennedy.pesquisagamesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisagamesurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
