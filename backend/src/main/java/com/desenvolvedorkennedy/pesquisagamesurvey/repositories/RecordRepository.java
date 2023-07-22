f8cbf8d3a0624a0d39e782a1f0aa4d203ab23f96
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-07-22 10:27:37 -0300

    Get records

--- backend/src/main/java/com/desenvolvedorkennedy/pesquisagamesurvey/repositories/RecordRepository.java
+++ backend/src/main/java/com/desenvolvedorkennedy/pesquisagamesurvey/repositories/RecordRepository.java
@@ -1,6 +1,12 @@
 package com.desenvolvedorkennedy.pesquisagamesurvey.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.desenvolvedorkennedy.pesquisagamesurvey.entities.Record;
@@ -8,4 +14,26 @@ import com.desenvolvedorkennedy.pesquisagamesurvey.entities.Record;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
     
+    Page<Record> findByMomentBetweenOrderByMomentDesc(Instant startMoment, Instant endMoment, Pageable pageable);
+
+    @Query("SELECT obj FROM Record obj WHERE" 
+        + "(:min IS NULL OR obj.moment >= :min) AND"
+        + "(:max IS NULL OR obj.moment <= :max)")
+    Page<Record> findByMoments(@Param("min") Instant min,@Param("max")  Instant max, Pageable pageable);
+
+    @Query("SELECT MIN(r.moment) FROM Record r")
+    Instant findMinMoment();
+
+    @Query("SELECT MAX(r.moment) FROM Record r")
+    Instant findMaxMoment();
+
+    @Query(nativeQuery = true, value = 
+        "SELECT obj.* FROM tb_record obj "
+        + "WHERE (:min IS NULL OR obj.moment >= :min) AND (:max IS NULL OR obj.moment <= :max) "
+        + "ORDER BY obj.moment DESC OFFSET :offset ROWS FETCH NEXT :pageSize ROWS ONLY")
+    Page<Record> findByMomentsSql(@Param("min") Instant min, 
+                        @Param("max") Instant max, 
+                        @Param("offset") int offset, 
+                        @Param("pageSize") int pageSize, 
+                        @Param("pageable") Pageable pageable);
 }
--- backend/src/main/java/com/desenvolvedorkennedy/pesquisagamesurvey/services/RecordService.java
+++ backend/src/main/java/com/desenvolvedorkennedy/pesquisagamesurvey/services/RecordService.java
@@ -3,14 +3,18 @@ package com.desenvolvedorkennedy.pesquisagamesurvey.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.desenvolvedorkennedy.pesquisagamesurvey.entities.Record;
 import com.desenvolvedorkennedy.pesquisagamesurvey.dto.RecordDTO;
 import com.desenvolvedorkennedy.pesquisagamesurvey.dto.RecordInsertDTO;
-import com.desenvolvedorkennedy.pesquisagamesurvey.repositories.RecordRepository;
+import com.desenvolvedorkennedy.pesquisagamesurvey.entities.Record;
 import com.desenvolvedorkennedy.pesquisagamesurvey.repositories.GameRepository;
+import com.desenvolvedorkennedy.pesquisagamesurvey.repositories.RecordRepository;
 
 @Service
 public class RecordService {
@@ -30,5 +34,14 @@ public class RecordService {
         entity.setGame(gameRepository.getReferenceById(dto.getGameId()));
         return new RecordDTO(recordRepository.save(entity));
     }
-    
+
+    @Transactional(readOnly = true)
+    public Page<RecordDTO> findByMomentsBetweenOrder(Instant startMoment, Instant endMoment, PageRequest pageRequest) {
+
+        Instant defaultMinDate = startMoment != null ? startMoment : recordRepository.findMinMoment();
+        Instant defaultMaxDate = endMoment != null ? endMoment : Instant.now();
+
+        return recordRepository.findByMomentBetweenOrderByMomentDesc(defaultMinDate, defaultMaxDate, pageRequest).map(x -> new RecordDTO(x));
+    }
+
 }
